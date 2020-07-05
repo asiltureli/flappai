8573439138e76d9425298a217cc058c0fc4ed4a8
Mert Asil Tuereli <asil.tuereli@rwth-aachen.de>
2020-07-06 00:03:07 +0200

    Game objects added

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ var config = {
     physics: {
         default: 'arcade',
         arcade:  {
-            gravity: {y : 500},
+            gravity: {y : 0},
             debug: false
         }
     },
@@ -21,19 +21,43 @@ var game = new Phaser.Game(config);
 function preload ()
 {
     this.load.image('sky', 'assets/sky.png');
-    this.load.image('pipe', 'assets/pipe-green.png');
+    this.load.image('pipe_top', 'assets/pipe_top.png');
+    this.load.image('pipe_bottom', 'assets/pipe_bottom.png');
     this.load.image('bird', 'assets/redbird.png');
 }
 
 function create ()
 {
-
+    // Create Starting Images
     this.add.image(0,0, 'sky').setOrigin(0,0);
-    this.add.image(50, 300, 'bird');
 
+    // Create Random Generated Pipes
     pipes = this.physics.add.staticGroup();
+
+    for(var x_pos = 300; x_pos < 748; x_pos += 175)
+    {
+        var top_int = Math.floor(Math.random()*400) + 100;
+        var bottom_int = 600 - top_int - 100;
+        pipes.create(x_pos, top_int/2, 'pipe_top').setScale(1, top_int/320).refreshBody();
+        pipes.create(x_pos, 600-bottom_int/2, 'pipe_bottom').setScale(1,bottom_int/320).refreshBody();
+    }
+   // Create Bird Object 
+   bird = this.physics.add.sprite(50, 300, 'bird');
+   bird.setCollideWorldBounds(true);
+   bird.body.setGravityY(500);
+
+   // Add Collider
+   this.physics.add.collider(bird, pipes);
+
 }
 
 function update ()
 {
+    // Bird can be controlled with space key
+    cursors = this.input.keyboard.createCursorKeys();
+
+    if (cursors.space.isDown)
+    {
+        bird.setVelocityY(-300);
+    }
 }
